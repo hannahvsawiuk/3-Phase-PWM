fd1abcee46c880be5439bc083c9882a18f7ff61f
Hannah Sawiuk <hannahvsawiuk@gmail.com>
2017-08-13 15:47:15 -0700

    Working with potentiometer

--- Analog Input.c
+++ Analog Input.c
@@ -1,24 +1,19 @@
 /* analogRead() converts the input voltage (0 to 5 volt range) to a digital value between 0 and 1023
  * Want a frequency variation of 0 to 120 Hz, so 
  */
- 
-#define inputPin A0
 int analogIn = 0;
 float frequency = 0.0; 
 String str2;
 String str1 = String("Frequency: ");
 
 void setup() {
-  pinMode(analogIn, INPUT);
+  Serial.begin(9600); //configure Baud rate
 }
 
 void loop() {
-  analogIn = analogRead(inputPin);
-  if(analogIn < (1023/120)) //gives range to frequency to be zero
-    frequency = 0.0;
-  else 
-    frequency = analogIn * (120/1023);
-   
-  str2 = str1 + frequency;
-  Serial.println(str2);
+  analogIn = analogRead(A0);   //read analog data from pin A0
+  frequency = (analogIn * 120.0)/1023.0; //convert to a frequency
+  str2 = str1 + frequency + '\r'; //concatenate string
+  Serial.print(str2); //Print to the console. Using PuTTY as the console
+  delay(200); //delay for 200 ms
 }
